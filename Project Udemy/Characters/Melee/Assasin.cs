a2dffa747f15604f5e552c7f657a6091ff8811be
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-02-24 14:02:32 +0100

    Assasin

--- Project Udemy/Characters/Melee/Assasin.cs
+++ Project Udemy/Characters/Melee/Assasin.cs
@@ -20,5 +20,117 @@ namespace Project_Udemy.Characters.Melee
         private LightLeatherVest bodyArmor;
         private Sword weapon;
 
+
+        public int AbilityPoints
+        {
+            get
+            {
+                return abilityPoints;
+            }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    abilityPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 10.");
+                }
+            }
+        }
+        public int HealthPoints
+        {
+            get
+            {
+                return healthPoints;
+            }
+            set
+            {
+                if (value >= 0 && value <= 50)
+                {
+                    healthPoints = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Inappropriate value, the value should be >= 0 and <= 50");
+                }
+            }
+        }
+        public string Faction
+        {
+            get
+            {
+                return faction;
+            }
+            set
+            {
+                if (value == "Male" || value == "Spellcaster")
+                {
+                    faction = value;
+                }
+                else
+                {
+                    throw new ArgumentException(string.Empty, "The faction should be either male");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length <= 3 && value.Length <= 12)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException(string.Empty, "Inappropriate length of the name, should be between 3 and 12");
+                }
+            }
+        }
+        
+        public LightLeatherVest BodyArmor
+        {
+            get
+            {
+                return bodyArmor;
+            }
+            set
+            {
+                bodyArmor = value;
+            }
+        }
+        
+        public Sword Weapon
+        {
+            get
+            {
+                return weapon;
+            }
+            set
+            {
+                weapon = value;
+            }
+        }
+
+        public void Raze()
+        {
+            throw new NotImplementedException();
+        }
+        public void BleedToDeath()
+        {
+            throw new NotImplementedException();
+        }
+        public void Survival()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
