0fb049a7e9aa9e908d3db28b4eeddd2789d46640
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-03-08 14:05:26 +0100

    .

---
+++ Project Udemy/Playersinfo.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace Project_Udemy
+{
+    public static  class Playersinfo
+    {
+        private static string playersInfoDirectory = "";
+        private static string[,] fullInfo;
+        private static int[] scores;
+        private static int[] levels;
+
+
+        public static void Initialize()
+        {
+
+        }
+
+        public static void Save()
+        {
+
+        }
+
+        public static void UpdateFullInfo()
+        {
+
+        }
+        public static void RetrieveFullInfo()
+        {
+
+        }
+        public static void PrintfFullInfo()
+        {
+
+        }
+        public static void EraseFullInfo()
+        {
+
+        }
+        public static void UpdateScores()
+        {
+
+        }
+        public static void RetrieveScores()
+        {
+
+        }
+        public static void PrintScores()
+        {
+
+        }
+        public static void EraseScores()
+        {
+
+        }
+        public static void UpdateLevels()
+        {
+
+        }
+        public static void RetrieveLevels()
+        {
+
+        }
+        public static void PrintLevels()
+        {
+
+        }
+        public static void EraseLevels()
+        {
+
+        }
+    }
+
+}
